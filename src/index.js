51a5f9c7917939992796c55819e444c6bf984df9
Viral Vaghela <viralvaghela8577@gmail.com>
2024-03-31 19:20:38 +0530

    add - user and video model

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import connectDB from "./db/index.js";
 import app from "./app.js";
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, () =>  {
       console.log(`Server is running at port ${process.env.PORT}`);
     });
     app.on("error", (error) => {
